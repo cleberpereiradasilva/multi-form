9c8d246945df370a58ce4e73bfef0784c4fe65da
Cleber Silva <noct@mackbook.local>
2023-07-08 15:06:20 -0300

    feat: wip in first step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,112 @@
-import Image from 'next/image'
+type StepProps = {
+  step: number;
+  description: string;
+  active?: boolean;
+};
+
+const SideStep = ({ step, description, active }: StepProps) => (
+  <div className="flex items-center mb-5">
+    <div>
+      <div
+        className={`md:h-8 md:w-8 sm:w-8 sm:h-8 font-bold text-sm ${
+          active
+            ? "bg-blue-light"
+            : " text-gray-light border-2 border-gray-cool"
+        } rounded-full flex items-center justify-center mr-3`}
+      >
+        {step}
+      </div>
+    </div>
+    <div className="sm:hidden md:block">
+      <div className="text-gray-cool text-sm">STEP {step}</div>
+      <div className="font-bold text-gray-light -tracking-tighter text-sm">
+        {description}
+      </div>
+    </div>
+  </div>
+);
+
+type InputProps = {
+  label?: string;
+  placeholder?: string;
+  name?: string;
+  id?: string;
+  value?: string;
+  validator?: void;
+  message?: string;
+};
+
+const Input = ({
+  label,
+  placeholder,
+  name,
+  id,
+  value,
+  validator,
+  message,
+}: InputProps) => (
+  <div className="flex flex-col mt-7">
+    <div className="flex flex-row justify-between">
+      <label className="text-blue-marine font-bold pb-1">{label}</label>
+      <label className="font-bold text-sm text-red-strawberry pr-1">
+        {message && message}
+      </label>
+    </div>
+    <input
+      className="border-2 rounded-lg p-2"
+      type="text"
+      placeholder={placeholder}
+      name={name}
+      id={id || name}
+      value={value || ""}
+    />
+  </div>
+);
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex bg-blue-light font-ubuntu text-base min-h-screen sm:flex-row md:flex-col md:items-center sm:items-stretch  justify-center md:p-5 sm:p-0">
+      <div className="sm:bg-sm-side items-stretch sm:bg-blue-light md:bg-none  bg-no-repeat bg-top lg:w-930 sm:w-full  md:w-full md:h-184 sm:min-h-full md:bg-white sm:p-0 md:p-2 rounded-lg flex sm:flex-col md:flex-row lg:flex-row">
+        <div className="lg:w-side p-5 md:w-side bg-no-repeat sm:h-19 md:h-full sm:bg-none md:bg-side rounded-lg sm:pt-12 md:pt-10 pl-8 flex md:flex-col sm:flex-row md:justify-start sm:justify-center">
+          <SideStep step={1} description={"YOUR INFO"} active />
+          <SideStep step={2} description={"SELECT PLAN"} />
+          <SideStep step={3} description={"ADD ONS"} />
+          <SideStep step={4} description={"SUMARY"} />
+        </div>
+        <div className="h-full w-full flex sm:justify-between md:justify-normal flex-col">
+          <div className="flex flex-col justify-center sm:pb-8 md:pb-3 sm:m-6 md:m-0 lg:w-form md:w-4/5 sm:p-5 md:pl-24 sm:pt-5 md:pt-11 md:pr-24 text-gray-cool bg-white sm:rounded-lg sm:shadow-2xl">
+            <div className="sm:pr-30">
+              <div className="sm:text-2xl md:text-4xl font-bold text-blue-marine">
+                Personal info
+              </div>
+              <span className="text-sm">
+                Pelase provider your name, email address, and phone number.
+              </span>
+              <Input
+                label="Name"
+                name="name"
+                placeholder="e.g. Stephen King"
+                message="Erro on validate"
+              />
+              <Input
+                label="Email"
+                name="name"
+                placeholder="e.g.stephenking@lorem.com"
+              />
+              <Input
+                label="Phone Number"
+                name="phonenumber"
+                placeholder="e.g. +1 23 456 7890"
+              />
+            </div>
+          </div>
+          <div className="flex flex-row justify-end justify-items-center md:h-full  md:pb-2 lg:w-form md:w-4/5   sm:p-3 sm:pr-6 md:pt-19 md:pr-24 text-gray-cool bg-white">
+            <button className="p-2 sm:w-28 md:w-36 h-12 text-gray-light bg-blue-marine sm:rounded-md md:rounded-xl">
+              Next Step
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,46 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '375px',
+        md: '650px',
+        lg: '1000px'
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu', ' sans-serif']
+      },
       backgroundImage: {
+        side: "url('./assets/images/bg-sidebar-desktop.svg')",
+        'sm-side': "url('./assets/images/bg-sidebar-mobile.svg')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      spacing: {
+        side: '375px',
+        form: '645px',
+        19: '4.7rem',
+        184: '35.5rem',
+        92: '23.5rem',
+        930: '930px',
+        600: '600px'
+      },
+      colors: {
+        'blue-marine': 'hsl(213, 96%, 18%)',
+        'blue-purplish': 'hsl(228, 100%, 84%)',
+        'blue-light': 'hsl(206, 94%, 87%)',
+        'red-strawberry': 'hsl(354, 84%, 57%)',
+        'gray-cool': 'hsl(231, 11%, 63%)',
+        'gray-light': 'hsl(229, 24%, 87%)',
+        magnolia: 'hsl(217, 100%, 97%)',
+        alabaster: 'hsl(231, 100%, 99%)',
+        white: 'hsl(0, 0%, 100%)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
